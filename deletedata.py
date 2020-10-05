0b92334a4b97de114e5f13dbf552ae0e92e6cd70
av-dx <aashwinvaish1@gmail.com>
2020-10-06 00:16:34 +0530

    Added delContact()

---
+++ deletedata.py
@@ -0,0 +1,50 @@
+from searchdata import SearchCust
+from texttable import Texttable
+
+
+def delContact(cur, con):
+    try:
+        name = input(
+            "Search for the customer name whose contact you want to delete : ")
+        SearchCust(name, cur, con)
+        cust_id = int(
+            input("Enter the customer ID whose contact you want to delete : "))
+        cur.execute("SELECT * FROM CONTACT WHERE cust_id=%d" % (cust_id))
+        cust = []
+        todelete = []
+        table = Texttable()
+        table.header(["S.No.", "Phone"])
+        table.set_cols_dtype(["i", "t"])
+        for i, row in enumerate(cur):
+            cust.append(row)
+            table.add_row([i+1, row['phone']])
+        print(table.draw())
+        while (len(todelete) <= len(cust)):
+            sno = input(
+                "Enter the Sno of the Phone no. you want to delete, leave blank to quit :")
+            if sno:
+                sno = int(sno)
+                todel = cust[sno-1]
+                if (todel in todelete):
+                    print("Number already deleted!")
+                    continue
+                else:
+                    todelete.append(todel)
+                    cur.execute("DELETE FROM CONTACT WHERE cust_id=%d AND phone='%s'" % (
+                        cust_id, todel['phone']))
+                    continue
+            else:
+                break
+        if (len(todelete) > 0):
+            ch = input(
+                "Are you sure you want to delete the selected numbers? (y/N)")
+            if (ch.lower() == 'y'):
+                con.commit()
+                print("Contacts deleted!")
+            else:
+                con.rollback()
+                print("Contacts were not deleted.")
+    except Exception as e:
+        con.rollback()
+        print("Failed to delete contacts into database")
+        print(">>>>>>>>>>>>>", e)
