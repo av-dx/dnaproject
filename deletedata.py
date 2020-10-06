cb49137e22e73e66926e30d477198ae4ca78875a
av-dx <aashwinvaish1@gmail.com>
2020-10-06 12:41:03 +0530

    fixes to delSpecialGuest()

--- deletedata.py
+++ deletedata.py
@@ -69,44 +69,50 @@ def delReportsTo(agent_id, mgr_id, cur, con):
         con.rollback()
         print("Failed to delete reports_to record from database")
         print(">>>>>>>>>>>>>", e)
-       
 
-def deleteSpecialGuest(cur,con):
+
+def delSpecialGuest(cur, con):
     try:
         eventname = input("Enter the name of the Event: ")
-        SearchEvents(eventname,cur,con)
-        required_id = int(input("Enter the event ID you want to perform the operation in: "))
-        cur.execute("SELECT * FROM SPECIAL_GUEST WHERE event_id = %d" %(required_id))
+        SearchEvents(eventname, cur, con)
+        required_id = int(
+            input("Enter the event ID you want to perform the operation in: "))
+        cur.execute("SELECT * FROM SPECIAL_GUEST WHERE event_id = %d" %
+                    (required_id))
         spec = []
         todelete = []
         table = Texttable()
-        table.header(["Serial No.","Name"])
+        table.header(["Serial No.", "Name"])
         table.set_cols_dtype(["i", "t"])
-        for i,row in enumerate(cur):
+        for i, row in enumerate(cur):
             spec.append(row)
             table.add_row([i+1, row['name']])
+        if len(spec) == 0:
+            raise Exception("This event has no special guests stored!")
         print(table.draw())
         while(len(todelete) <= len(spec)):
-            sno = int(input("Enter the Serial No(From the list) of the name you want to delete | leave blank to quit: "))
-            if(sno <= len(spec)):
-                if sno:
-                    sno = int(sno)
-                    todel = spec[sno-1]
-                    if(todel in todelete):
-                        print("Special Guest already deleted")
-                        continue
-                    else:
-                        todelete.append(todel)
-                        cur.execute("DELETE FROM SPECIAL_GUEST WHERE event_id = %d AND name = %s" %(required_id,todel['name']))
-                        continue
+            sno = input(
+                "Enter the Serial No(From the list) of the name you want to delete | leave blank to quit: ")
+            if sno:
+                sno = int(sno)
+                if(sno <= len(spec)):
+                    print("Serial No. out of range!")
+                    continue
+                todel = spec[sno-1]
+                if(todel in todelete):
+                    print("Special Guest already deleted")
+                    continue
                 else:
-                    break
+                    todelete.append(todel)
+                    cur.execute("DELETE FROM SPECIAL_GUEST WHERE event_id = %d AND name = %s" % (
+                        required_id, todel['name']))
+                    continue
             else:
-                print("Serial Number out of Range! ")
                 break
         if (len(todelete) > 0):
-            ch = input("Are you sure you want to delete the Special Guest(y/n): ")
-            if(ch.lower()=='y'):
+            ch = input(
+                "Are you sure you want to delete the Special Guest(y/n): ")
+            if(ch.lower() == 'y'):
                 con.commit()
                 print("Not special anymore!")
             else:
@@ -115,7 +121,4 @@ def deleteSpecialGuest(cur,con):
     except Exception as e:
         con.rollback()
         print("Failed to delete Special Guest")
-        print(">>>>>>>>>>>>>",e)
-
-
-
+        print(">>>>>>>>>>>>>", e)
