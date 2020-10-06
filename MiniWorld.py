8c394f046ad056115589279e1639ec86a5e4f92f
av-dx <aashwinvaish1@gmail.com>
2020-10-06 16:11:58 +0530

    prettier menus

--- MiniWorld.py
+++ MiniWorld.py
@@ -108,27 +108,103 @@ def PartSearch():  # For now its just search on names,if we want we can make it 
     return
 
 
-def dispatch(ch):
-    """
-    Function that maps helper functions to option entered
-    """
-
+def insertMenu():
+    print("1. Insert a booking")
+    print("2. Insert Contacts")
+    print("3. Insert a new Customer")
+    print("4. Insert a new Employee")
+    print("5. Insert Special Guest to an Event")
+    print("6. Back")
+    ch = int(input("Enter choice> "))
+    sp.call('clear', shell=True)
     if(ch == 1):
         makeBooking(cur, con)
     elif(ch == 2):
-        avgBookingFees()
+        insertContact(cur, con)
+    elif(ch == 3):
+        insertCustomer(cur, con)
+    elif(ch == 4):
+        insertEmployee(cur, con)
+    elif(ch == 5):
+        insertSpecialGuest(cur, con)
+    else:
+        return
+
+
+def deleteMenu():
+    print("1. Delete Customer Contacts")
+    print("2. Delete Special Guest for an Event")
+    print("3. Delete Special Guest for an Event")
+    print("4. Back")
+    ch = int(input("Enter choice> "))
+    sp.call('clear', shell=True)
+    if(ch == 1):
+        delContact(cur, con)
+    elif(ch == 2):
+        delSpecialGuest(cur, con)
+    elif(ch == 3):
+        delEmployee(cur, con)
+    else:
+        return
+
+
+def searchMenu():
+    print("1. Search for Agent based on City")
+    print("2. Search for Employee by name")
+    print("3. Search for Employee by city")
+    print("4. Search for Events between dates")
+    print("5. Search for Customer by name")
+    print("6. Back")
+    ch = int(input("Enter choice> "))
+    sp.call('clear', shell=True)
+    if(ch == 1):
+        lsAgentByCity(cur, con)
+    elif(ch == 2):
+        name = input("Enter name to search for : ")
+        SearchEmp(name, cur, con)
     elif(ch == 3):
         lsEmpByCity(cur, con)
     elif(ch == 4):
-        lsAgentByCity(cur, con)
-    elif(ch == 5):
-        countEntities()
-    elif(ch == 6):
         lsEventBwDates(cur, con)
-    elif(ch == 7):
-        PartSearch()
-    elif(ch == 8):
-        UpdateData()
+    elif(ch == 5):
+        name = input("Enter name to search for : ")
+        SearchCust(name, cur, con)
+    else:
+        return
+
+
+def updateMenu():
+    print("1. Update a Customer record")
+    print("2. Update an Event record")
+    print("3. Back")
+    ch = int(input("Enter choice> "))
+    sp.call('clear', shell=True)
+    if(ch == 1):
+        modifyCustomer(cur, con)
+    elif(ch == 2):
+        modifyEvent(cur, con)
+    else:
+        return
+
+
+def adminMenu(ch):
+    if(ch == 1):
+        insertMenu()
+    elif(ch == 2):
+        deleteMenu()
+    elif(ch == 3):
+        searchMenu()
+    elif(ch == 4):
+        updateMenu()
+    else:
+        print("Error: Invalid Option")
+
+
+def custMenu(ch):
+    if(ch == 1):
+        lsAgentByCity(cur, con)
+    elif(ch == 2):
+        lsEventBwDates(cur, con)
     else:
         print("Error: Invalid Option")
 
@@ -137,13 +213,17 @@ def dispatch(ch):
 while(1):
     tmp = sp.call('clear', shell=True)
 
-    # Can be skipped if you want to hard core username and password
+    print(r"   ____    _        _         _    _      _   ")
+    print(r"  / ___|  / \      / \       | |  | |_ __| |  ")
+    print(r" | |     / _ \    / _ \      | |  | __/ _` |  ")
+    print(r" | |___ / ___ \  / ___ \     | |__| || (_| |_ ")
+    print(r"  \____/_/   \_\/_/   \_\    |_____\__\__,_(_)")
+    print(r"                                              ")
+
     username = input("Username: ")
     password = input("Password: ")
 
     try:
-        # Set db name accordingly which have been create by you
-        # Set host to the server's address if you don't want to use local SQL server
         con = pymysql.connect(host='localhost',
                               user=username,
                               password=password,
@@ -156,27 +236,44 @@ while(1):
         else:
             print("Failed to connect")
 
-        tmp = input("Enter any key to CONTINUE>")
+        if username == "customer":
+            print("Logged in as a customer.")
+            adminMode = 0
+        else:
+            print("Logged in as an admin.")
+            adminMode = 1
+
+        input("Enter any key to CONTINUE>")
 
         with con.cursor() as cur:
-            while(1):
-                tmp = sp.call('clear', shell=True)
-                print("1. Make a booking")
-                print("2. Display Average Booking Fee for a city")
-                print("3. Display Employees for a given city")
-                print("4. List Agents in a City")
-                print("5. Count Entities")
-                print("6. List Events between two Dates")
-                print("7. Search by partial text")
-                print("8. Update")
-                print("9. Logout")
-                ch = int(input("Enter choice> "))
-                tmp = sp.call('clear', shell=True)
-                if ch >= 9:
-                    break
-                else:
-                    dispatch(ch)
-                    tmp = input("Enter any key to CONTINUE>")
+            if adminMode:
+                while(1):
+                    sp.call('clear', shell=True)
+                    print("1. Insert Records")
+                    print("2. Delete Records")
+                    print("3. Search Records")
+                    print("4. Update Records")
+                    print("5. Logout")
+                    ch = int(input("Enter choice> "))
+                    sp.call('clear', shell=True)
+                    if ch >= 5:
+                        break
+                    else:
+                        adminMenu(ch)
+                        input("Enter any key to CONTINUE>")
+            else:
+                while(1):
+                    sp.call('clear', shell=True)
+                    print("1. Search for Agent based on City")
+                    print("2. Search for Events between dates")
+                    print("3. Logout")
+                    ch = int(input("Enter choice> "))
+                    sp.call('clear', shell=True)
+                    if ch >= 3:
+                        break
+                    else:
+                        custMenu(ch)
+                        input("Enter any key to CONTINUE>")
 
     except:
         tmp = sp.call('clear', shell=True)
